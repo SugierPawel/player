ecaed117b685832c56e8e81661281a6621b733dc
SugierPawel <sugier.pawel@gmail.com>
2022-07-14 13:26:40 +0200

    as

--- main.go
+++ main.go
@@ -316,7 +316,7 @@ func (l *updSource) InitRtpReader(sc *core.StreamConfig) {
 				kind = "video"
 			case 97:
 				kind = "audio"
-				log.Printf("InitRtpReader, sn: %s, kind: %s, n: %d, payload: %d", sn, kind, n, rtpPacket.Header.PayloadType)
+				log.Printf("InitRtpReader, sn: %s, kind: %s, n: %d, ts: %d, sn: %d", sn, kind, n, rtpPacket.Header.Timestamp, rtpPacket.Header.SequenceNumber)
 			}
 			/*if n > 30 && n < 500 {
 				kind = "audio"
