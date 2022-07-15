4bbdde4f595cedd128be03597e2a50c437c2d807
SugierPawel <sugier.pawel@gmail.com>
2022-07-15 15:12:03 +0200

    s

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ import (
 )
 
 const (
-	updWriteSleepTime = 1000000
+	updWriteSleepTime = 10000000
 	sleepTime         = time.Millisecond * 100
 	serverAddress     = "172.26.9.100"          //"172.26.9.100"
 	webSocketAddr     = serverAddress + ":2000" //"172.26.9.100:2000" "127.0.0.1:2000"
