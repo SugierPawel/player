a84a56528db6117621ed87c5a80f1f23b3e9aa4b
SugierPawel <sugier.pawel@gmail.com>
2022-07-16 16:09:22 +0200

    s

--- conf/polsat_webrtc.sh
+++ conf/polsat_webrtc.sh
@@ -44,7 +44,7 @@ ffmpeg \
 -i "$audio_in" \
 -map 0:v:0 \
 -c:v copy \
--f rtp -payload_type 96 -max_delay 0 -application lowdelay "$video_out" \
+-f rtp -payload_type 96 "$video_out" \
 -map 1:a:0 \
 -af 'adelay=0|0' \
 -c:a libopus -sample_fmt s16 -vbr off -application lowdelay -compression_level 10 -frame_duration 120 -apply_phase_inv 1 -strict -2 -ac 2 -b:a 16k \
